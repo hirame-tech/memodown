c6c5aae86506698b20d40e2290961e7f6713833f
hirame-tech <101329221+hirame-tech@users.noreply.github.com>
2023-06-20 05:25:00 +0900

    add Edit Area

--- app/src/main/java/com/example/memodown/MainActivity.java
+++ app/src/main/java/com/example/memodown/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.memodown;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+//import android.view.Menu;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.titlebar);
+        // アクションバーにツールバーをセッ
+        setSupportActionBar(toolbar);
+        // ツールバーに戻るボタンを設置
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
+    public boolean onSupportNavigateUp() {
+        finish();
+        return super.onSupportNavigateUp();
     }
 }
