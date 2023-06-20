d46df4e39b101aaa309e8789b1a4b33d6bbac926
hirame-tech <101329221+hirame-tech@users.noreply.github.com>
2023-06-20 09:16:49 +0900

    added allert when you save

--- app/src/main/java/com/example/memodown/MainActivity.java
+++ app/src/main/java/com/example/memodown/MainActivity.java
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 //import android.view.Menu;
 
 
@@ -15,16 +19,32 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Toolbar toolbar = findViewById(R.id.titlebar);
-        // アクションバーにツールバーをセッ
+        // set Toolbar on ActionBar
         setSupportActionBar(toolbar);
         // ツールバーに戻るボタンを設置
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayShowHomeEnabled(true);
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
+
+        findViewById(R.id.save).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Snackbar.make(v, "ボタン１が押されました", Snackbar.LENGTH_SHORT).show();
+                Toast myToast = Toast.makeText(
+                        getApplicationContext(),
+                        "saved",
+                        Toast.LENGTH_SHORT
+                );
+                myToast.show();
+            }
+        });
     }
+
+    //return button
     public boolean onSupportNavigateUp() {
         finish();
         return super.onSupportNavigateUp();
     }
+
 }
